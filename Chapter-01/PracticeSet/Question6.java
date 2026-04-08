5bccb65207cad1fea95d8cdecd8336b90c9ef329
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-09 00:28:19 +0530

    feat: Check weather a number is integer or not

---
+++ Chapter-01/PracticeSet/Question6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class question6 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter A number");
+        int num=sc.nextInt();
+
+        boolean b1=sc.nextInt();
+        System.out.println(b1);
+    }
+}
