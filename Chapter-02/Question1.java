2a75b5b6d7a36c4c7ea6cb45ebdba84619c3d81f
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-11 22:42:34 +0530

    feat: Calculating the result of expression

---
+++ Chapter-02/Question1.java
@@ -0,0 +1,8 @@
+public class Question1 {
+    public static void main(String[] args) {
+        // Calculating expression: 7/4 * 9/2 using float
+        float result = 7 / 4.0f * 9 / 2.0f;
+
+        System.out.println("Result: " + result);
+    }
+}
