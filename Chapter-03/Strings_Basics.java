9f6156782dab7cb37e3ac3bd9387a68125e6fbb0
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-12 22:53:17 +0530

    feat: demonstrate String creation using literals and the new keyword
    Starting Chapter 3: Strings. Created examples of String instantiation via literal and object constructor

---
+++ Chapter-03/Strings_Basics.java
@@ -0,0 +1,12 @@
+public class CallString {
+    public static void main(String[] args) {
+
+        // Creating string using constructor
+        String str = new String("Tushar");
+        System.out.println("Using constructor: " + str);
+
+        // Creating string using literal (recommended)
+        String name = "Tushar";
+        System.out.println("Using literal: " + name);
+    }
+}
