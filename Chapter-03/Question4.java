829b918a1d6339a097776d839d2f7187a2b12253
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-13 22:49:21 +0530

    feat: solve Ch3 Question 4 - detect double and triple spaces
    Used the .indexOf() method with specific starting offsets to locate multiple space occurrences within a string. This demonstrates advanced string searching techniques

---
+++ Chapter-03/Question4.java
@@ -0,0 +1,9 @@
+public class Question4 {
+    public static void main(String[] args) {
+        String newstring= "This string  Is    Conatining   the Lower And The  Upper Case";
+        System.out.println(newstring.indexOf("  ",12));
+        System.out.println(newstring.indexOf("   ",16));
+    
+}
+    
+}
