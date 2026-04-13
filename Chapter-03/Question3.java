a508fb2a6a21ba7697ae3159668e44a1646ec170
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-13 22:46:10 +0530

    feat: solve Ch3 Question 3 - implement letter template replacement
    Created a letter template with a placeholder and used the .replace() method to dynamically insert a name. This demonstrates practical string template handling

---
+++ Chapter-03/Question3.java
@@ -0,0 +1,8 @@
+public class Question3 {
+    public static void main(String[] args) {
+        String letter= "Dear <|name|>, Thanks A lot";
+        System.out.println(letter.replace("<|name|>", "Tushar"));
+    
+}
+    
+}
