cd936797660a4eca875377dd8d22af2c7e288f73
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-15 23:02:55 +0530

    feat: add Logical Operators practice to Chapter 4
    Created Operator.java to demonstrate boolean logic using AND (&&), OR (||), and NOT (!) operators. Included conditional statements to test various truth table scenarios

---
+++ Chapter-04/Operator.java
@@ -0,0 +1,26 @@
+public class Operator {
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+
+        if(a&&b){
+            System.out.println("Both Are true");
+        }
+        else{
+          System.out.println("One Of Them Is False Or Both Are False ");
+
+
+        if (b||b){
+            System.out.println("One Of Them Is True");
+        }else{
+            System.out.println("Both Are False");
+
+        System.out.println(!a);
+        System.out.println(!b);
+        }
+        
+    }
+    
+}
+}
